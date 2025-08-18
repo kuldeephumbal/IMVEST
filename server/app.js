5bbd09accaf496c3e8ae68bfe156d3cce626f4fd
kuldeepH17 <humbalkuldeep54@gamil.com>
2025-08-18 21:58:24 +0530

    Add package.json with initial dependencies and scripts for server setup

---
+++ server/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const app = express();
+const connectDB = require('./config/db');
+const swaggerDocs = require('./docs/swagger');
+
+// Middleware
+swaggerDocs(app);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files from uploads directory
+app.use('/uploads', express.static('uploads', {
+    setHeaders: (res, path) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    }
+}));
+
+// MongoDB Connection
+connectDB();
+
+// Routes
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
